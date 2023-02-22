01df3830b8af732102f8d73e42ba9a0477d39236
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-22 10:17:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import pandas as pd
 from data_visualization import regplot
 from linear_regression import LinearRegression
+from multiple_regression import MultipleRegression
+from linear_algebra import create_extended_matrix
 
 
 if __name__ == '__main__':
 
     # Preparing data for our model
-    raw_data = pd.read_csv('real_estate_price_size.csv')
+    raw_data = pd.read_csv('real_estate_price_size_year.csv')
     size = raw_data['size'].values.tolist()
     price = raw_data['price'].values.tolist()
+    year = raw_data['year'].values.tolist()
 
     # Linear Regression model
     lr = LinearRegression()
     alpha, beta = lr.train(size, price)
 
+    # Multiple Regression model
+    mr = MultipleRegression()
+    features = create_extended_matrix([size, year])
+    mr.train(features, price)
+
     # Results
     regplot(size, price, alpha, beta)
     print(f"r_squared = {lr.r_squared(size, price)}")
+    print(f"multiple_r_squared = {mr.r_squared(features, price)}")
